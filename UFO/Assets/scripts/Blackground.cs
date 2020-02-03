9f4c5dda2033e801b20826906e9bc791059ddc54
kevin099191 <57181260+kevin099191@users.noreply.github.com>
2020-02-03 21:36:41 +0800

    場景完成

---
+++ UFO/Assets/scripts/Blackground.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class Blackground : MonoBehaviour
+{
+
+    public float speed=0.13f;
+    public Material bg_material;
+    private void Start()
+    {
+        bg_material = GetComponent<Renderer>().material;
+    }
+    private void Update()
+    {
+        float offset = Time.time * speed;
+
+        bg_material.SetTextureOffset("_MainTex", new Vector2(0, -offset));
+    }
+
+
+
+
+}
--- UFO/Assets/scripts/CWeapon.cs
+++ UFO/Assets/scripts/CWeapon.cs
@@ -29,57 +29,57 @@ public class CWeapon : MonoBehaviour
         {
             
             //Instantiate(ufo, point[0].GetComponent<Transform>().position, Quaternion.identity);
-            Instantiate(ufo, point[0].position, Quaternion.identity);
+            Instantiate(ufo, point[0].position, ufo.transform.rotation);
             
         }
 
         if (Input.GetKeyDown(KeyCode.Keypad2))
         {
             //Instantiate(ufo, point[1].GetComponent<Transform>().position, Quaternion.identity);
-            Instantiate(ufo, point[1].position, Quaternion.identity);
+            Instantiate(ufo, point[1].position, ufo.transform.rotation);
         }
 
         if (Input.GetKeyDown(KeyCode.Keypad3))
         {
             //Instantiate(ufo, point[2].GetComponent<Transform>().position, Quaternion.identity);
-            Instantiate(ufo, point[2].position, Quaternion.identity);
+            Instantiate(ufo, point[2].position, ufo.transform.rotation);
         }
 
     
         if (Input.GetKeyDown(KeyCode.Keypad4))
         {
             //Instantiate(ufo, point[3].GetComponent<Transform>().position, Quaternion.identity);
-            Instantiate(ufo, point[3].position, Quaternion.identity);
+            Instantiate(ufo, point[3].position, ufo.transform.rotation);
         }
 
         if (Input.GetKeyDown(KeyCode.Keypad5))
         {
             //nstantiate(ufo, point[4].GetComponent<Transform>().position, Quaternion.identity);
-            Instantiate(ufo, point[4].position, Quaternion.identity);
+            Instantiate(ufo, point[4].position, ufo.transform.rotation);
         }
 
         if (Input.GetKeyDown(KeyCode.Keypad6))
         {
             //Instantiate(ufo, point[5].GetComponent<Transform>().position, Quaternion.identity);
-            Instantiate(ufo, point[5].position, Quaternion.identity);
+            Instantiate(ufo, point[5].position, ufo.transform.rotation);
         }
 
         if (Input.GetKeyDown(KeyCode.Keypad7))
         {
             // Instantiate(ufo, point[6].GetComponent<Transform>().position, Quaternion.identity);
-            Instantiate(ufo, point[6].position, Quaternion.identity);
+            Instantiate(ufo, point[6].position, ufo.transform.rotation);
         }
 
         if (Input.GetKeyDown(KeyCode.Keypad8))
         {
             //  Instantiate(ufo, point[7].GetComponent<Transform>().position, Quaternion.identity);
-            Instantiate(ufo, point[7].position, Quaternion.identity);
+            Instantiate(ufo, point[7].position, ufo.transform.rotation);
         }
 
         if (Input.GetKeyDown(KeyCode.Keypad9))
         {
             //  Instantiate(ufo, point[8].GetComponent<Transform>().position, Quaternion.identity);
-            Instantiate(ufo, point[8].position, Quaternion.identity);
+            Instantiate(ufo, point[8].position, ufo.transform.rotation);
         }
 
 
